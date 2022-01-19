392ec618e6ed1b8130acb9277f379fa4fae94bac
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-01-19 13:16:48 -0500

    add comments

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,12 +3,17 @@ import classNames from "classnames";
 import "components/InterviewerListItem.scss";
 
 function InterviewerListItem(props) {
+  {/*link with classes*/}
+  
   let interviewClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected,
   });
   let interviewerImg = classNames("interviewers__item-image", {
     "interviewers__item--selected-image": props.selected,
   });
+
+
+  {/*Add an event handler; Render the interviewer's name*/}
   return (
     <li
       onClick={() => props.setInterviewer(props.id)}
