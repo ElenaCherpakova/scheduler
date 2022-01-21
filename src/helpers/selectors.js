af542a1a15787685fc25a043470a776915e0ac55
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-01-20 20:50:29 -0500

    fix  helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,19 +1,15 @@
-
 export function getAppointmentsForDay(state, day) {
-//there are no appointments on the given day, our days data will be empty.
-  if (!state.days) {
-    return [];
-  }
-  let filteredDay = state.days.filter(d => d.name === day)[0];
+  //there are no appointments on the given day, our days data will be empty.
+
+  let filteredDay = state.days.filter((d) => d.name === day)[0];
   if (!filteredDay) {
     return [];
   }
-//comparing it's id matches the id of states.appointments 
+  //comparing it's id matches the id of states.appointments
   let result = [];
-  for (const id of filteredDay.appointments) {
-    const appointmentObj = state.appointments[id];
-    result.push(appointmentObj);
+  if (filteredDay.appointments.length > 0) {
+    for (let id of filteredDay.appointments)
+      result.push(state.appointments[id]);
   }
   return result;
 }
-
