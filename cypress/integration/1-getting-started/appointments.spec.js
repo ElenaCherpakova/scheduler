e67b1a936f24b23eed2fcd019cc880b80bd82aa2
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-01-26 21:46:55 -0500

    implement end-to-end test, update package json

--- cypress/integration/1-getting-started/appointments.spec.js
+++ cypress/integration/1-getting-started/appointments.spec.js
@@ -32,15 +32,13 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Tori Malcolm");
   });
   it("should cancel an interview", () => {
-    cy.get("[alt=Delete]")
-      .click({ force: true });
-  
+    cy.get("[alt=Delete]").click({ force: true });
+
     cy.contains("Confirm").click();
-  
+
     cy.contains("Deleting").should("exist");
     cy.contains("Deleting").should("not.exist");
-  
-    cy.contains(".appointment__card--show", "Archie Cohen")
-      .should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
   });
 });
